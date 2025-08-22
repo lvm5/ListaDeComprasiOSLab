a29cb8eca39414ad9896bcbfef76777a6e137ca3
Sonzzini <paulo.sonzzini@gmail.com>
2025-08-21 21:13:05 -0300

    Initial Commit

---
+++ ListaDeCompras/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ListaDeCompras
+//
+//  Created by Paulo Sonzzini Ribeiro de Souza on 21/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
