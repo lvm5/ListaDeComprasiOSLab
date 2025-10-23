dc6157e0500309ab8faf6d13e7514ecf73612027
Leandro Vansan <lvm5@icloud.com>
2025-10-23 18:43:11 -0300

    Feat: Lista de produtos com opção de tirar foto ou escolher da galeria (incompleto)

---
+++ ListaDeCompras/Category/UpdateCategorySheetView.swift
@@ -0,0 +1,18 @@
+//
+//  UpdateCategorySheetView.swift
+//  ListaDeCompras
+//
+//  Created by Leandro Vansan de Morais on 2025-10-23.
+//
+
+import SwiftUI
+
+struct UpdateCategorySheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdateCategorySheetView()
+}
---
+++ ListaDeCompras/Extensions/ImagePickerCoordinator.swift
@@ -0,0 +1,39 @@
+import SwiftUI
+import UIKit
+
+struct ImagePickerCoordinator: UIViewControllerRepresentable {
+    
+    @Binding var selectedImage: UIImage?
+    @Environment(\.presentationMode) private var presentationMode
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera // abre a câmera
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        var parent: ImagePickerCoordinator
+        
+        init(_ parent: ImagePickerCoordinator) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(
+            _ picker: UIImagePickerController,
+            didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]
+        ) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
